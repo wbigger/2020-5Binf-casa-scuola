64616cc64bf152927e16b97d8fb38ce08d2ac492
Claudio <claudio.capobianco@marconicloud.it>
2021-01-13 09:41:58 +0100

    Add input text and submit button

---
+++ app.js
@@ -0,0 +1,11 @@
+function queryClass() {
+    let response = {
+        action: "Devi andare a scuola!!!"
+    }
+
+    console.log("va tutto bene!");
+    
+    $("#response").text(response.action);
+  }
+
+$(document).ready();
